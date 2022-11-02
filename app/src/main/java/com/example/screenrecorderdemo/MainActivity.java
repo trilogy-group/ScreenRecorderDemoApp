518b98043a5833ef18b81f2ad67b0ba5605ccb86
Raul Guerrero <raul.guerrero@devfactory.com>
2022-11-01 21:34:04 -0600

    no message

---
+++ app/src/main/java/com/example/screenrecorderdemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.screenrecorderdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // this code opens the PWA on Chrome
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://trilogy-group.github.io"));
+            startActivity(browserIntent);
+        });
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        TextView res = (TextView) findViewById(R.id.screenshot_path);
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+        if (appLinkData != null) {
+            res.setText(appLinkData.getQueryParameter("folder"));
+        }
+    }
+}
